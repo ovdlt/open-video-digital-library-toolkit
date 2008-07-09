dcd6acf7acda3a17278aaf9e7158f22d37eafe93
CDD Developer <cdd@ketchup.local>
2008-07-08 17:21:14 -0700

    Add mirror 'vendor/plugin/rspec_rails/'.

---
+++ vendor/plugin/rspec_rails/lib/spec/rails.rb
@@ -0,0 +1,14 @@
+silence_warnings { RAILS_ENV = "test" }
+
+require 'application'
+require 'action_controller/test_process'
+require 'action_controller/integration'
+require 'active_record/fixtures' if defined?(ActiveRecord::Base)
+require 'test/unit'
+
+require 'spec'
+
+require 'spec/rails/matchers'
+require 'spec/rails/mocks'
+require 'spec/rails/example'
+require 'spec/rails/extensions'
