4d04ed5521392c5212210f668903a9cb6f60b3f5
Will Leinweber <will@bitfission.com>
2008-07-18 16:29:03 -0700

    revert factories.rb

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,17 +1,12 @@
-# Factory.sequence :filename do |n|
-#   filename = "temp_video_#{n}.mov"
-#   create_temp_video filename
-#   filename
-# end
+Factory.sequence :filename do |n|
+  filename = "temp_video_#{n}.mov"
+  create_temp_video filename
+  filename
+end
 
 Factory.define :video do |v|
   v.title    "Our Mr. Sun"
-  # v.filename { Factory.next :filename }
-  v.filename do
-    filename = "temp_video_our_mr_sun.mov"
-    create_temp_video filename
-    filename
-  end
+  v.filename { Factory.next :filename }
   v.sentence "This film describes the sun in scientific but entertaining terms."
   v.size     261.megabytes
 end
