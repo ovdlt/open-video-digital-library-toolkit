a2a71af9f8a0ada6bef9c5c00bbe81122332c570
Steven Parkes <smparkes@smparkes.net>
2008-08-04 11:02:03 -0700

    Update mirror 'vendor/plugins/rspec_rails/' to HEAD.

--- vendor/plugins/rspec_rails/lib/spec/rails.rb
+++ vendor/plugins/rspec_rails/lib/spec/rails.rb
@@ -1,6 +1,6 @@
 silence_warnings { RAILS_ENV = "test" }
 
-require 'application'
+require_dependency 'application'
 require 'action_controller/test_process'
 require 'action_controller/integration'
 require 'active_record/fixtures' if defined?(ActiveRecord::Base)
@@ -12,3 +12,4 @@ require 'spec/rails/matchers'
 require 'spec/rails/mocks'
 require 'spec/rails/example'
 require 'spec/rails/extensions'
+require 'spec/rails/interop/testcase'
