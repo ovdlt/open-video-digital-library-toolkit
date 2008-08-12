8439bba1c0c33e779bc374b2d5afa90dc4f99a96
Steven Parkes <smparkes@smparkes.net>
2008-08-12 09:56:22 -0700

    turn of caching in dev mode (was used for debugging)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,7 +12,7 @@ config.whiny_nils = true
 config.action_controller.consider_all_requests_local = true
 config.action_view.debug_rjs                         = true
 config.action_controller.perform_caching             = false
-config.action_controller.perform_caching             = true
+# config.action_controller.perform_caching             = true
 
 
 # Don't care if the mailer can't send
