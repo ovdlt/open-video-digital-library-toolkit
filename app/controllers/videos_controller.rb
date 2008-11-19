1b850976740b497fbd43ff3606e2d662a867ea03
Steven Parkes <smparkes@smparkes.net>
2008-11-19 10:10:04 -0800

    make visibility changes admin-only

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -125,132 +125,6 @@ class VideosController < ApplicationController
     _change
   end
   
-  def _change
-
-    @rollback = false
-    @new = {}
-
-    Video.transaction do
-
-      okay = true
-
-      if v = params[:video]
-        v[:assets]
-        if a = v[:assets]
-          a.reject! { |val| val == ":id:" }
-          v[:asset_ids] = a.map { |s| s.to_i }
-          v.delete(:assets)
-        end
-        
-        okay = false if !@video.update_attributes(v)
-      end
-
-      if ps = params[:property]
-        ps.each do |p_id,p_params|
-          p_ar = @properties.detect { |p_ar| p_ar.id == p_id.to_i }
-          if p_ar
-            if p_params["deleted"] == "deleted"
-              p_ar.destroy
-            else
-              p_params.delete "deleted"
-              # pp p_ar.errors if !p_ar.update_attributes(p_params)
-              okay = false if !p_ar.update_attributes(p_params)
-            end
-          elsif p_id =~ /^new(_[a-z]+)?_\d+$/ and p_params["deleted"] != "deleted"
-            p_params.delete "deleted"
-            @properties << (p = @video.properties.build p_params)
-            @new[p_id] = p
-            if !p.save
-              okay = false
-            end
-            # raise p.errors.inspect if !p.save
-          elsif p_id !~ /new(_[a-z]+)?/ and p_params["deleted"] != "deleted"
-            logger.warn "bad p id: #{p_id}"
-            render :nothing => true, :status => 400
-            raise ActiveRecord::Rollback
-          end
-        end
-
-      end
-
-      if params[:new_assets]
-        params[:new_assets].each do |uri|
-          @video.assets << Asset.new( :uri => uri )
-        end
-      end
-
-      if dts = params["descriptor_type"]
-
-        dts.each do |dt,dvs|
-
-          dt_id = dt.to_i
-          
-          if dt_id.nil? or dt_id < 1
-            render_bad_request 
-            return
-          end
-          
-          current_ids = @properties.select { |p| p.property_type_id == dt_id }.map(&:integer_value)
-          on_ids = dvs.select { |k,v| v == "1" }.map { |k,v| k.to_i }
-          off_ids = dvs.select { |k,v| v == "no" }.map { |k,v| k.to_i }
-          
-          turn_on = on_ids - current_ids
-          turn_off = off_ids & current_ids
-          
-          turn_off.each do |dv_id|
-            destroy_dv_property( dt_id, dv_id )
-          end
-
-          turn_on.each do |dv_id|
-            create_dv_property( dt_id, dv_id )
-          end
-
-        end
-
-      end
-
-      if params["descriptor_value"]
-        @video.descriptors = params["descriptor_value"].map do |d|
-          begin
-            DescriptorValue.find d.to_i
-          rescue ActiveRecord::RecordNotFound
-            render_bad_request 
-            return
-          end
-        end
-      end
-
-      was_new = @video.new_record?
-      
-      @video.save
-
-      if okay
-        if was_new
-          flash[:notice] = "#{@video.title} was added"
-          if params["submit"] == "save"
-            redirect_to edit_video_path( @video )
-          else
-            redirect_to videos_path
-          end
-        else
-          flash[:notice] = "#{@video.title} saved"
-          if params["submit"] == "save"
-            redirect_to edit_video_path( @video )
-          else
-            redirect_to video_path( @video )
-          end
-        end
-      else
-        @rollback = true
-        flash[:error] = "Errors exist; could not update"
-        render :action => :form
-        raise ActiveRecord::Rollback
-      end
-
-    end
-
-  end
-
   def edit
     @object = @video = Video.find( params[:id] )
     render :action => :form
@@ -385,6 +259,136 @@ class VideosController < ApplicationController
   def destroy_dv_property pt_id, dv_id
     @properties.reject! { |p| p.property_type_id == pt_id and p.integer_value == dv_id }
     Property.find_by_video_id_and_property_type_id_and_integer_value( @video.id, pt_id, dv_id ).destroy
+  end
+
+  def _change
+
+    @rollback = false
+    @new = {}
+
+    Video.transaction do
+
+      okay = true
+
+      if v = params[:video]
+
+        if a = v[:assets]
+          a.reject! { |val| val == ":id:" }
+          v[:asset_ids] = a.map { |s| s.to_i }
+          v.delete(:assets)
+        end
+        
+        if !current_user.has_role? :admin
+          v.delete( :public )
+        end
+
+        okay = false if !@video.update_attributes(v)
+      end
+
+      if ps = params[:property]
+        ps.each do |p_id,p_params|
+          p_ar = @properties.detect { |p_ar| p_ar.id == p_id.to_i }
+          if p_ar
+            if p_params["deleted"] == "deleted"
+              p_ar.destroy
+            else
+              p_params.delete "deleted"
+              # pp p_ar.errors if !p_ar.update_attributes(p_params)
+              okay = false if !p_ar.update_attributes(p_params)
+            end
+          elsif p_id =~ /^new(_[a-z]+)?_\d+$/ and p_params["deleted"] != "deleted"
+            p_params.delete "deleted"
+            @properties << (p = @video.properties.build p_params)
+            @new[p_id] = p
+            if !p.save
+              okay = false
+            end
+            # raise p.errors.inspect if !p.save
+          elsif p_id !~ /new(_[a-z]+)?/ and p_params["deleted"] != "deleted"
+            logger.warn "bad p id: #{p_id}"
+            render :nothing => true, :status => 400
+            raise ActiveRecord::Rollback
+          end
+        end
+
+      end
+
+      if params[:new_assets]
+        params[:new_assets].each do |uri|
+          @video.assets << Asset.new( :uri => uri )
+        end
+      end
+
+      if dts = params["descriptor_type"]
+
+        dts.each do |dt,dvs|
+
+          dt_id = dt.to_i
+          
+          if dt_id.nil? or dt_id < 1
+            render_bad_request 
+            return
+          end
+          
+          current_ids = @properties.select { |p| p.property_type_id == dt_id }.map(&:integer_value)
+          on_ids = dvs.select { |k,v| v == "1" }.map { |k,v| k.to_i }
+          off_ids = dvs.select { |k,v| v == "no" }.map { |k,v| k.to_i }
+          
+          turn_on = on_ids - current_ids
+          turn_off = off_ids & current_ids
+          
+          turn_off.each do |dv_id|
+            destroy_dv_property( dt_id, dv_id )
+          end
+
+          turn_on.each do |dv_id|
+            create_dv_property( dt_id, dv_id )
+          end
+
+        end
+
+      end
+
+      if params["descriptor_value"]
+        @video.descriptors = params["descriptor_value"].map do |d|
+          begin
+            DescriptorValue.find d.to_i
+          rescue ActiveRecord::RecordNotFound
+            render_bad_request 
+            return
+          end
+        end
+      end
+
+      was_new = @video.new_record?
+      
+      @video.save
+
+      if okay
+        if was_new
+          flash[:notice] = "#{@video.title} was added"
+          if params["submit"] == "save"
+            redirect_to edit_video_path( @video )
+          else
+            redirect_to videos_path
+          end
+        else
+          flash[:notice] = "#{@video.title} saved"
+          if params["submit"] == "save"
+            redirect_to edit_video_path( @video )
+          else
+            redirect_to video_path( @video )
+          end
+        end
+      else
+        @rollback = true
+        flash[:error] = "Errors exist; could not update"
+        render :action => :form
+        raise ActiveRecord::Rollback
+      end
+
+    end
+
   end
 
 end
