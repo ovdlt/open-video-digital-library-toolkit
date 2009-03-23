709e2909c28d64617da369e1ff2ce837eb6858b4
Steven Parkes <smparkes@smparkes.net>
2009-03-22 17:19:48 -0700

    fix hard-coded root path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,7 +15,7 @@ class SessionsController < ApplicationController
       self.current_user = user
       new_cookie_flag = (params[:remember_me] == "1")
       handle_remember_cookie! new_cookie_flag
-      redirect_back_or_default('/')
+      redirect_back_or_default( root_path )
       flash[:notice] = "Logged in successfully"
     else
       note_failed_signin
@@ -28,7 +28,7 @@ class SessionsController < ApplicationController
   def destroy
     logout_killing_session!
     flash[:notice] = "You have been logged out."
-    redirect_back_or_default('/')
+    redirect_back_or_default( root_path )
   end
 
 protected
