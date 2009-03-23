5866d9e78314fb27a08335779b5e739f45032fc6
Steven Parkes <smparkes@smparkes.net>
2009-03-22 18:16:21 -0700

    rails 2.3 changes

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -134,20 +134,20 @@ describe UsersController do
     
     it "should route users_path() to /users" do
       users_path().should == "/users"
-      formatted_users_path(:format => 'xml').should == "/users.xml"
-      formatted_users_path(:format => 'json').should == "/users.json"
+      users_path(:format => 'xml').should == "/users.xml"
+      users_path(:format => 'json').should == "/users.json"
     end
     
     it "should route new_user_path() to /users/new" do
       new_user_path().should == "/users/new"
-      formatted_new_user_path(:format => 'xml').should == "/users/new.xml"
-      formatted_new_user_path(:format => 'json').should == "/users/new.json"
+      new_user_path(:format => 'xml').should == "/users/new.xml"
+      new_user_path(:format => 'json').should == "/users/new.json"
     end
     
     it "should route user_(:id => '1') to /users/1" do
       user_path(:id => '1').should == "/users/1"
-      formatted_user_path(:id => '1', :format => 'xml').should == "/users/1.xml"
-      formatted_user_path(:id => '1', :format => 'json').should == "/users/1.json"
+      user_path(:id => '1', :format => 'xml').should == "/users/1.xml"
+      user_path(:id => '1', :format => 'json').should == "/users/1.json"
     end
     
     it "should route edit_user_path(:id => '1') to /users/1/edit" do
