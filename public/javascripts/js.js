ca6cdfe91f1cf30acbc3b028d58d93f728dd4417
Gary Geisler <ggeisler@gmail.com>
2010-01-15 17:14:12 -0600

    Make minor styling tweaks to home page.

--- public/javascripts/js.js
+++ public/javascripts/js.js
@@ -168,6 +168,6 @@ jQuery(document).ready(function() {
 });
 
 jQuery(document).ready( function() {
-    setTimeout( 'jQuery(".flash").effect("highlight", {}, 500 );', 500 );
-    setTimeout( 'jQuery(".flash.notice").fadeOut( 600 );' , 600 );
+    setTimeout( 'jQuery(".flash").effect("highlight", {}, 1000 );', 500 );
+    setTimeout( 'jQuery(".flash.notice").fadeOut( 2000 );' , 2000 );
 } );
