d2f3cac10fe6ec5f149dd3a0ba0420d894adf884
Steven Parkes <smparkes@smparkes.net>
2010-02-02 15:34:32 -0800

    more install help stuff

--- Rakefile
+++ Rakefile
@@ -29,9 +29,20 @@ task :release do
   rm_rf "pkg"
   mkdir_p "pkg/ovdlt"
   system "cp -r . pkg/ovdlt"
+
+  rm_f "pkg/ovdlt/config/initializers/site_keys.rb"
+  rm_f "pkg/ovdlt/config/database.yml"
+  rm_f "pkg/ovdlt/coverage/*"
   rm_rf "pkg/ovdlt/pkg"
+  cmd = "rm -rf pkg/ovdlt/tmp/* pkg/ovdlt/tmp/.* pkg/ovdlt/public/surrogates pkg/ovdlt/public/assets"
+  puts cmd
+  system cmd
+  system 'find pkg/ovdlt/log -name \\*.log| xargs rm -f'
+  system 'find vendor -name \\*.o -o -name \\*.bundle -o -name \\*.so | xargs rm -f'
+  system 'find vendor -name Makefile | fgrep /ext/ | xargs rm -f'
+
   rm_f "pkg/ovdlt/ovdlt.tgz"
   mkdir_p "pkg/ovdlt/public/surrogates pkg/ovdlt/public/assets"
   system "find pkg -name .git | xargs rm -rf"
-  system "(cd pkg; tar cvfz ../ovdlt.tgz ovdlt)"
+  system "(cd pkg; tar cf - ovdlt | gzip -9 > ../ovdlt.tgz)"
 end
--- init.rb
+++ init.rb
@@ -2,15 +2,44 @@
 
 require 'fileutils'
 
-if !ARGV[0]
-  print "usage: ruby init.rb <directory_name>\n"
+tarfile = ARGV[0]
+dir = ARGV[1]
+
+if !tarfile || !dir
+  print "usage: ruby init.rb <tarfile> <directory_name>\n"
   exit 0
 end
 
 include FileUtils
 
-mkdir_p ARGV[0]
-system "tar Cxvfpz #{ARGV[0]} ~/ovdlt/ovdlt.tgz"
-chdir ARGV[0]
+mkdir_p dir
+system "cat #{tarfile} | tar Cxfpz #{dir} -"
+chdir dir
 system "mv ovdlt/{*,.[a-zA-Z]*} ."
 rmdir "ovdlt"
+system "sed -e s/ovdlt/#{dir}/g -e s/-development//g -e s/-test//g -e s/-production//g config/database.yml.sample > config/database.yml"
+text = (1..16).collect { (i = Kernel.rand(62); i += ((i < 10) ? 48 : ((i < 36) ? 55 : 61 ))).chr }.join
+system "test -e config/initializers/site_keys.rb || sed -e 's/CHANGE THIS!!!!!!!!/#{text}/' config/initializers/site_keys.rb.example > config/initializers/site_keys.rb"
+
+cmd = "grep relative_url_root config/environment.rb || sed -e 's/:user_observer/:user_observer\\n\\nconfig.action_controller.relative_url_root = \"\\/#{dir}\"/' -i config/environment.rb"
+puts cmd
+system cmd
+
+cmd = "grep :sendmail config/environment.rb || echo 'ActionMailer::Base.delivery_method = :sendmail' >> config/environment.rb"
+puts cmd
+system cmd
+
+cmd = "grep :location config/environment.rb || echo 'ActionMailer::Base.sendmail_settings[:location] = %(/usr/bin/sendmail)' >> config/environment.rb"
+puts cmd
+system cmd
+
+system "rake gems:build"
+ENV["RAILS_ENV"] = "production"
+system "rake db:create"
+system "rake db:migrate"
+
+system "rake spec:db:fixtures:load"
+cp_r "spec/fixtures/assets/.", "public/assets/."
+cp_r "spec/fixtures/surrogates/.", "public/surrogates/."
+
+system "rake db:populate"
