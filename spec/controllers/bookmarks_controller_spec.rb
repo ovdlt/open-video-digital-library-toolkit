79d5b2866c1e0b3deafe4bc74c9ba2273e14d61d
Gary Geisler <ggeisler@gmail.com>
2010-03-08 15:49:26 -0600

    Update spec/fixtures to get tests and updated fixtures in sync to pass tests.

--- spec/controllers/bookmarks_controller_spec.rb
+++ spec/controllers/bookmarks_controller_spec.rb
@@ -15,22 +15,22 @@ describe BookmarksController do
     end
 
     it "should require all items owned user" do
-      login_as_10
+      login_as_3
       put :order, :order => "1 8"
       response.should be_missing
     end
 
     it "should reorder the list" do
-      login_as_10
+      login_as_3
 
-      Collection.find(1).all_videos.map(&:id).should == [ 1, 2, 3, 4, 5, 6, 7 ]
-      Collection.find(1).all_bookmarks.map(&:id).should == [ 2, 1, 3, 4, 5, 6, 7 ]
+      Collection.find(1).all_videos.map(&:id).should == [ 1, 2, 3, 4, 5 ]
+      Collection.find(1).all_bookmarks.map(&:id).should == [ 2, 1, 3, 4, 5 ]
 
-      put :order, :order => [ 2, 1, 3, 4, 5, 6, 7 ].reverse.join(" ")
+      put :order, :order => [ 2, 1, 3, 4, 5 ].reverse.join(" ")
       response.should be_success
 
-      Collection.find(1).all_videos.map(&:id).should == [ 1, 2, 3, 4, 5, 6, 7 ].reverse
-      Collection.find(1).all_bookmarks.map(&:id).should == [ 2, 1, 3, 4, 5, 6, 7 ].reverse
+      Collection.find(1).all_videos.map(&:id).should == [ 1, 2, 3, 4, 5 ].reverse
+      Collection.find(1).all_bookmarks.map(&:id).should == [ 2, 1, 3, 4, 5 ].reverse
     end
 
   end
--- spec/models/permission_spec.rb
+++ spec/models/permission_spec.rb
@@ -3,7 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe Permission do
   before(:each) do
     @valid_attributes = {
-      :user_id => 1,
+      :user_id => 5,
       :role_id => 1
     }
   end
